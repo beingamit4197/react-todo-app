7ea56da758803c0c651a902d8fef0116d0362a55
Amit Rawat <beingamit4197@gmail.com>
2022-08-12 15:07:09 +0530

    created and modified

--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo';
 import Todoform from './Todoform'
 
 function Todolist() {
@@ -12,11 +13,25 @@ const addTodo = todo => {
     const newTodos = [todo, ...todos];
 
     setTodos(newTodos);
-}
+};
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+          if(todo.id ) {
+            todo.isComplete = !todo.isComplete;
+          }
+      return todo;
+        });
+    setTodos(updatedTodos);
+};
+
     return (
     <div>
         <h1>What's the plan for today?</h1>
         <Todoform onSubmit={addTodo}/>
+        <Todo 
+        todos={todos}
+        completeTodo={completeTodo} />
     </div>
   )
 }
